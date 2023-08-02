7961d0b82df039018174f803b8505d84f921ca97
Megantee1805 <megan.tee1805@gmail.com>
2023-08-02 22:28:35 +0800

    updated

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -7,6 +7,28 @@ import { sendToVercelAnalytics } from './vitals';
 import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { createRoot } from 'react-dom/client';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/database'; // For Realtime Database
+// import 'firebase/firestore'; // For Firestore
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_ID,
+  appId: process.env.REACT_APP_FIREBASE_MESSAGE_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log(app)
+
 
 const root = createRoot(document.getElementById('root'));
 
